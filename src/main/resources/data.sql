6581c8da19219b36f08d686dd36430664ef54db1
juanlg <juanlg@pc8-gsl>
2017-12-21 17:09:13 +0100

    Actualizo datos de la bbdd

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO user (id, username,first_name, last_name) VALUES
   (1, 'user1','lala', 'lala'),
-  (2, 'user2','lolo', 'lolo');
+  (2, 'user2','lele', 'lele'),
+  (3, 'user3','lili', 'lili');
