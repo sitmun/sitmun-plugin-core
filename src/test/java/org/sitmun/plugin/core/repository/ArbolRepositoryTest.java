527815a2943c7d2e148b81daf680a8a4d492c5d7
rrioja <rodolfo@geoslab.com>
2018-04-09 14:05:37 +0200

    Subo test de unidad para entidades Arbol y Cargo

---
+++ src/test/java/org/sitmun/plugin/core/repository/ArbolRepositoryTest.java
@@ -0,0 +1,53 @@
+package org.sitmun.plugin.core.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.sitmun.plugin.core.domain.Arbol;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ArbolRepositoryTest {
+    
+    @Autowired
+    private ArbolRepository repository;
+ 
+    private Arbol item;
+    
+    @Before
+    public void init() {
+        item = new Arbol();
+        item.setId(1);
+        item.setNombre("Test");
+        
+    }
+
+    @Test
+    public void addItem() throws JsonProcessingException {
+        Iterable<Arbol> persistentItems = repository.findAll();
+        assertThat(persistentItems).hasSize(0);
+        repository.save(item);
+        System.out.println(this.serialize(item));
+        persistentItems = repository.findAll();
+        assertThat(persistentItems).hasSize(1);                
+        
+    }
+    
+    private byte[] serialize(Object object) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsBytes(object);
+    }
+
+}
