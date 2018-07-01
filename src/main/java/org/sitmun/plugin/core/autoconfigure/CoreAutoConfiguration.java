3054c3e9693eb99f71b3a28f752177d9ddc57f27
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2018-07-01 16:41:34 +0200

    Removed dependency of H2 in implementation

--- src/main/java/org/sitmun/plugin/core/autoconfigure/CoreAutoConfiguration.java
+++ src/main/java/org/sitmun/plugin/core/autoconfigure/CoreAutoConfiguration.java
@@ -1,19 +1,8 @@
 package org.sitmun.plugin.core.autoconfigure;
 
-import org.h2.server.web.WebServlet;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CoreAutoConfiguration {
     
-    
-    @Bean
-    ServletRegistrationBean h2servletRegistration(){
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
-        registrationBean.addUrlMappings("/h2-console/*");
-        return registrationBean;
-    }
-
 }
