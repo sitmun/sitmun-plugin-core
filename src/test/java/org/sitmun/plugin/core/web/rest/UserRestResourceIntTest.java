c4524b4abbb877666b30e788156b1c5c8b602d67
rrioja <rodolfo@geoslab.com>
2018-07-04 17:31:55 +0200

    UserRestResourceIntTest fix

--- src/test/java/org/sitmun/plugin/core/web/rest/UserRestResourceIntTest.java
+++ src/test/java/org/sitmun/plugin/core/web/rest/UserRestResourceIntTest.java
@@ -1,11 +1,16 @@
 package org.sitmun.plugin.core.web.rest;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.sitmun.plugin.core.domain.Cartography;
-import org.sitmun.plugin.core.domain.Territory;
 import org.sitmun.plugin.core.domain.User;
 import org.sitmun.plugin.core.repository.UserRepository;
 import org.sitmun.plugin.core.security.SecurityConstants;
@@ -19,21 +24,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.transaction.annotation.Transactional;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import java.util.HashMap;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import org.junit.runners.MethodSorters;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @AutoConfigureMockMvc
 public class UserRestResourceIntTest {
 
@@ -65,11 +57,10 @@ public class UserRestResourceIntTest {
 
 	//private static final String DEFAULT_USER_URI = "http://localhost/api/users/1";
 	private static final String USER_URI = "http://localhost/api/users";
-	private static final String NEW_USER_URI = "http://localhost/api/users/1";
+	//private static final String NEW_USER_URI = "http://localhost/api/users/1";
 
 	@Before
 	public void init() {
-		//userRepository.deleteAll();
 		token = tokenProvider.createToken(USER_USERNAME);
 		user = new User();
 		user.setAdministrator(USER_ADMINISTRATOR);
@@ -82,14 +73,12 @@ public class UserRestResourceIntTest {
 	}
 
 	@Test
-	@Transactional
 	public void createUser() throws Exception {
-
-		mvc.perform(post("/api/users").header(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token)
+		userRepository.deleteAll();
+		String uri = mvc.perform(post("/api/users").header(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token)
 				.contentType(MediaType.APPLICATION_JSON_UTF8).content(Util.convertObjectToJsonBytes(user)))
-				.andExpect(status().isCreated()).andExpect(header().string("Location", is(NEW_USER_URI)));
-
-		mvc.perform(get(NEW_USER_URI ).header(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token))
+				.andExpect(status().isCreated()).andReturn().getResponse().getHeader("Location");
+		mvc.perform(get(uri ).header(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token))
 				.andExpect(status().isOk()).andExpect(content().contentType(Util.APPLICATION_HAL_JSON_UTF8))
 				.andExpect(jsonPath("$.username", equalTo(USER_USERNAME)));
 
